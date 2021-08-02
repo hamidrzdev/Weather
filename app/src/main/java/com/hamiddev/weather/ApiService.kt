c72c0a3a840fa15e107c194443a26a205c68b1e8
hamidrzdev <hrk1380@gmail.com>
2021-08-02 14:02:28 +0430

    init-api_service_di_commit

---
+++ app/src/main/java/com/hamiddev/weather/ApiService.kt
@@ -0,0 +1,10 @@
+package com.hamiddev.weather
+
+import kotlinx.coroutines.flow.Flow
+import retrofit2.http.GET
+
+interface ApiService {
+
+    
+
+}
---
+++ app/src/main/java/com/hamiddev/weather/di/ApiServiceDi.kt
@@ -0,0 +1,29 @@
+package com.hamiddev.weather.di
+
+import com.hamiddev.weather.ApiService
+import com.hamiddev.weather.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiServiceDi {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService =
+        retrofit.create(ApiService::class.java)
+}
