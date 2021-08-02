12ec6b80fb06ae19266537569c76550d41cd8a31
hamidrzdev <hrk1380@gmail.com>
2021-08-02 15:43:22 +0430

    fix_data_source_package_commit

--- app/src/main/java/com/hamiddev/data/WeatherDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.hamiddev.data
-
-import com.hamiddev.model.WeatherResponse
-import kotlinx.coroutines.flow.Flow
-import org.neshan.common.model.LatLng
-
-interface WeatherDataSource {
-    fun getWeather(latLng: LatLng, accessToken: String): Flow<WeatherResponse>
-}
