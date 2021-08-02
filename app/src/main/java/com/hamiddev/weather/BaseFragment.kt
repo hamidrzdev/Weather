402dd7c7c817c0d306b5d07b0327bbb78742a5f3
hamidrzdev <hrk1380@gmail.com>
2021-08-02 13:13:18 +0430

    base_fragment_commit

---
+++ app/src/main/java/com/hamiddev/weather/BaseFragment.kt
@@ -0,0 +1,44 @@
+package com.hamiddev.weather
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
+
+@AndroidEntryPoint
+abstract class BaseFragment<VM : ViewModel, VB : ViewBinding>(
+    private var inflate: Inflate<VB>
+) : Fragment() {
+
+    var binding: VB? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = inflate.invoke(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    open fun initView() {
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
